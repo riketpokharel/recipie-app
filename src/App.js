963c62f7f0beb14a1fd9bdf1d553ea2781ae76b8
Riket Pokharel <pokharelriket777@gmail.com>
2023-09-10 23:48:17 +0545

    final recipie-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import Featured from "./components/Featured";
+import TopNav from "./components/TopNav";
+import Delivery from "./components/Delivery";
+import TopPics from "./components/TopPics";
+import Meal from "./components/Meal";
+import Categories from "./components/Categories";
+import NewsLetter from "./components/NewsLetter";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TopNav />
+      <Featured />
+      <Delivery />
+      <TopPics />
+      <Meal />
+      <Categories />
+      <NewsLetter />
+      <Footer />
     </div>
   );
 }
